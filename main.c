b5dd3960b8930bb70c32a2fe85f0e9711aa23d59
Daan van Oost <daanvanoost2005@gmail.com>
2025-09-17 14:34:16 +0200

    My fourth commit

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main()
 {
-<<<<<<< HEAD
-   printf("Hello, Solarsystem!"); 
-=======
    printf("Hello, Galaxy!"); //person 1
->>>>>>> 478f02cd9bd34e14b2727b40adbdd705eb62ef44
    return 0;
 }
